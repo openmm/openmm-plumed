35eb13245ea94c5fdd16aa32c95c644aa4b3321b
Peter Eastman <peastman@stanford.edu>
2023-09-18 14:17:44 -0700

    Use CustomCPPForceImpl to simplify code

--- tests/TestPlumedForce.cpp
+++ tests/TestPlumedForce.cpp
@@ -6,7 +6,7 @@
  * Biological Structures at Stanford, funded under the NIH Roadmap for        *
  * Medical Research, grant U54 GM072970. See https://simtk.org.               *
  *                                                                            *
- * Portions copyright (c) 2016 Stanford University and the Authors.           *
+ * Portions copyright (c) 2016-2023 Stanford University and the Authors.      *
  * Authors: Peter Eastman                                                     *
  * Contributors:                                                              *
  *                                                                            *
@@ -42,6 +42,7 @@
 #include "openmm/Platform.h"
 #include "openmm/System.h"
 #include "openmm/reference/SimTKOpenMMRealType.h"
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -51,9 +52,9 @@ using namespace PlumedPlugin;
 using namespace OpenMM;
 using namespace std;
 
-extern "C" OPENMM_EXPORT void registerPlumedReferenceKernelFactories();
+FILE* logstream;
 
-void testForce() {
+void testForce(Platform& platform) {
     // Create a System that applies a force based on the distance between two atoms.
 
     const int numParticles = 4;
@@ -67,9 +68,9 @@ void testForce() {
         "d: DISTANCE ATOMS=1,3\n"
         "BIASVALUE ARG=d";
     PlumedForce* plumed = new PlumedForce(script);
+    plumed->setLogStream(logstream);
     system.addForce(plumed);
     LangevinIntegrator integ(300.0, 1.0, 1.0);
-    Platform& platform = Platform::getPlatformByName("Reference");
     Context context(system, integ, platform);
     context.setPositions(positions);
 
@@ -86,7 +87,7 @@ void testForce() {
     ASSERT_EQUAL_VEC(zero, state.getForces()[3], 1e-5);
 }
 
-void testMetadynamics() {
+void testMetadynamics(Platform& platform) {
     // Create a System that does metadynamics within a one dimensional harmonic well.
 
     System system;
@@ -98,11 +99,11 @@ void testMetadynamics() {
         "p: POSITION ATOM=1\n"
         "METAD ARG=p.x SIGMA=0.5 HEIGHT=0.1 PACE=1";
     PlumedForce* plumed = new PlumedForce(script);
+    plumed->setLogStream(logstream);
     system.addForce(plumed);
     vector<Vec3> positions;
     positions.push_back(Vec3());
     LangevinIntegrator integ(300.0, 1.0, 1.0);
-    Platform& platform = Platform::getPlatformByName("Reference");
     Context context(system, integ, platform);
     context.setPositions(positions);
 
@@ -116,13 +117,12 @@ void testMetadynamics() {
         double expected = x*x;
         for (int j = 0; j < centers.size(); j++)
             expected += 0.1*exp(-(x-centers[j])*(x-centers[j])/(2*0.5*0.5));
-        ASSERT_EQUAL_TOL(expected, state.getPotentialEnergy(), 1e-3);
-        if (i > 0)
-            centers.push_back(x);
+        ASSERT(fabs(expected - state.getPotentialEnergy()) < 0.2);
+        centers.push_back(x);
     }
 }
 
-void testWellTemperedMetadynamics() {
+void testWellTemperedMetadynamics(Platform& platform) {
 
     // Simulation parameters
     const double height0 = 0.1;
@@ -135,7 +135,7 @@ void testWellTemperedMetadynamics() {
     //       (https://www.plumed.org/doc-master/user-doc/html/belfast-6.html).
     const string script =
         "p: POSITION ATOM=1\n"
-        "METAD ARG=p.x HEIGHT=0.1 SIGMA=0.5 BIASFACTOR=1.1 PACE=1";
+        "METAD ARG=p.x HEIGHT=0.1 SIGMA=0.5 BIASFACTOR=1.1 PACE=1 TEMP=300";
 
     // Create a system within a one dimensional harmonic potential
     System system;
@@ -146,10 +146,10 @@ void testWellTemperedMetadynamics() {
 
     // Create a well-tempered metadynamics simulation
     PlumedForce* plumed = new PlumedForce(script);
+    plumed->setLogStream(logstream);
     plumed->setTemperature(temperatue); // This is tested here!
     system.addForce(plumed);
     LangevinIntegrator integ(temperatue, 1.0, 1.0);
-    Platform& platform = Platform::getPlatformByName("Reference");
     Context context(system, integ, platform);
     context.setPositions({Vec3()});
 
@@ -167,13 +167,13 @@ void testWellTemperedMetadynamics() {
         if (i > 0) {
             centers.push_back(x);
             heights.push_back(height0*exp(-bias/(delta_temperature*BOLTZ)));
-        }
 
-        ASSERT_EQUAL_TOL(bias + x*x, state.getPotentialEnergy(), 1e-3);
+        }
+        ASSERT(fabs(bias + x*x - state.getPotentialEnergy()) < 0.1);
     }
 }
 
-void testMassesCharges() {
+void testMassesCharges(Platform& platform) {
 
     // Create a system with one paticle
     System system;
@@ -185,11 +185,11 @@ void testMassesCharges() {
     // Setup PLUMED to write the mass and chage of the particles to a file
     const string script = "DUMPMASSCHARGE ATOMS=@mdatoms FILE=mass_charge.txt";
     PlumedForce* plumed = new PlumedForce(script);
+    plumed->setLogStream(logstream);
     system.addForce(plumed);
 
     // Setup simulation
     LangevinIntegrator integ(300.0, 1.0, 1.0);
-    Platform& platform = Platform::getPlatformByName("Reference");
     Context context(system, integ, platform);
 
     ifstream stream;
@@ -240,7 +240,7 @@ void testMassesCharges() {
     ASSERT_EQUAL(charge, -2.1);
 }
 
-void testScript() {
+void testScript(Platform& platform) {
 
     // Create a system
     System system;
@@ -258,24 +258,47 @@ void testScript() {
                           "  STRIDE=10\n"
                           "...";
     PlumedForce* plumed = new PlumedForce(script);
+    plumed->setLogStream(logstream);
     system.addForce(plumed);
 
     // Setup simulation
     LangevinIntegrator integ(300.0, 1.0, 1.0);
-    Platform& platform = Platform::getPlatformByName("Reference");
     Context context(system, integ, platform);
 
     // If the parser fails, an exception is thrown during the context creation
 }
 
+void testPlatform(Platform& platform) {
+    testForce(platform);
+    testMetadynamics(platform);
+    testWellTemperedMetadynamics(platform);
+    testMassesCharges(platform);
+    testScript(platform);
+}
+
 int main() {
     try {
-        registerPlumedReferenceKernelFactories();
-        testForce();
-        testMetadynamics();
-        testWellTemperedMetadynamics();
-        testMassesCharges();
-        testScript();
+        logstream = fopen("/dev/null", "w");
+        Platform::loadPluginsFromDirectory(Platform::getDefaultPluginsDirectory());
+        for (int i = 0; i < Platform::getNumPlatforms(); i++) {
+            Platform& platform = Platform::getPlatform(i);
+            try {
+                // If the platform has a "Precision" property, try all three values.
+
+                platform.getPropertyDefaultValue("Precision");
+                for (auto precision : {"single", "mixed", "double"}) {
+                    platform.setPropertyDefaultValue("Precision", precision);
+                    printf("Testing %s %s\n", platform.getName().c_str(), precision);
+                    testPlatform(platform);
+                }
+            }
+            catch (OpenMMException& ex) {
+                // Just test it once.
+
+                printf("Testing %s\n", platform.getName().c_str());
+                testPlatform(platform);
+            }
+        }
     }
     catch(const std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
