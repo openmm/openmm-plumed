2fcb3bca487dc6db281ec8b26d2ab1c78896486f
peastman <peastman@stanford.edu>
2023-11-07 12:31:47 -0800

    Removed old code

--- devtools/scripts/install_cuda.sh
+++ devtools/scripts/install_cuda.sh
@@ -12,12 +12,6 @@ echo 'APT::Acquire::Retries "5";' | sudo tee /etc/apt/apt.conf.d/80-retries
 sudo apt-key del 7fa2af80
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
-
-#sudo wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries 5 \
-#    -O /etc/apt/preferences.d/cuda-repository-pin-600 \
-#    https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-#sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update -qq
 
 CUDA_APT=${CUDA_VERSION/./-}
